31a7d11c8c6cde9836163dd62711f06c67887f63
atilolaabdullahi6-wq <atilolabdullahi6@gmail.com>
2025-12-01 01:02:11 +0100

    last commit

--- index.js
+++ index.js
@@ -19,6 +19,30 @@ function animate(){
         ctx.fillStyle = "black";
         x = radius; // Correct position
     } 
+
+
+    
+ctx.fillStyle = "green";
+ctx.fillRect(100,100, 300, 300);
+// ROOF
+ctx.fillStyle = "red";
+ctx.beginPath();
+ctx.moveTo(100, 100);
+ctx.lineTo(400, 100);
+ctx.lineTo(250, 0);
+ctx.fill();
+//WINDOW
+ctx.fillStyle = "blue"
+ctx.fillRect(150, 150, 50, 50);
+ctx.fillRect(300, 150, 50, 50);
+ctx.fillRect(150, 225, 50, 50);
+ctx.fillRect(300, 225, 50, 50);
+ctx.fillRect(150, 300, 50, 50);
+ctx.fillRect(300, 300, 50, 50);
+//DOOR
+ctx.fillStyle = "yellow"
+ctx.fillRect(225, 300, 50, 100) 
+
     requestAnimationFrame(animate);
 }
 animate();
